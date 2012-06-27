f0cbd41f2ac2d8482074862dd73c23ed3d588386
Ken Mazaika <kmazaika@gmail.com>
2012-06-27 19:38:58 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LolcommitServer::Application.config.secret_token = '31df01d8d4e8e7b15eb62cf7739df764070c030ba9833089d037a214715d9f547c2c9d9998f2ae58135f86a3ddd54e836a69f242878007d6e54c5e32bd126af5'

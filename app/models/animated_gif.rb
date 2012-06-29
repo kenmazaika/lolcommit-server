5ecbb3f87b8bbd908bfe19144bd8d57bd829b2da
Ken Mazaika <kmazaika@gmail.com>
2012-06-29 08:24:32 -0400

    Before create store the image; add test

--- app/models/animated_gif.rb
+++ app/models/animated_gif.rb
@@ -7,22 +7,25 @@ class AnimatedGif < ActiveRecord::Base
   # TODO: this code needs tests, also should be
   # refactored.  Don't pipe to shell to create
   # directory.  Don't redundantly determine urls.
+
+  private
+
   def store_animation
     split_shas = self.shas.split(',')
     commits = GitCommit.where(:sha => split_shas)
     id = UUID.generate(:compact)
-    `mkdir #{Rails.root}/tmp/#{id}`
+    `mkdir #{directory}`
     commits.collect do |commit|
       image_req = HTTParty.get(commit.image.to_s)
       if image_req.success?
-        File.open( "#{Rails.root}/tmp/#{id}/#{commit.sha}.jpg", "wb") do |image|
+        File.open( "#{directory}/#{commit.sha}.jpg", "wb") do |image|
           image << image_req.body
         end
       end
     end
 
     images = split_shas.collect do |sha|
-      path = "#{Rails.root}/tmp/#{id}/#{sha}.jpg"
+      path = "#{directory}/#{sha}.jpg"
       if File.exist?(path)
         path
       else
@@ -32,12 +35,20 @@ class AnimatedGif < ActiveRecord::Base
 
     animation = ImageList.new(*images)
     animation.delay = 75
-    animation.write("#{Rails.root}/tmp/#{id}.gif")
-    @file = File.open("#{Rails.root}/tmp/#{id}.gif")
+    animation.write("#{Rails.root}/tmp/#{uuid}.gif")
+    @file = File.open("#{Rails.root}/tmp/#{uuid}.gif")
     self.image = @file
   end
 
   def close_file
     @file.close if @file
+  end
+
+  def directory
+    @directory ||= "#{Rails.root}/tmp/#{uuid}"
+  end
+
+  def uuid
+    @uuid ||= UUID.generate(:compact)
   end
 end

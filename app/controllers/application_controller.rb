fb0710613cbd8ab79d96f361219924f7e42dd94b
Ken Mazaika <kmazaika@gmail.com>
2012-06-30 21:23:44 -0400

    Hook up user logic to application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,32 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :validate_api_credentials_good
+
+  private
+
+  def validate_api_credentials_good
+    if ! params[:key].blank?
+      t = params[:t]
+      token = params[:token]
+
+      user = User.find_by_api_key(params[:key])
+      if user.blank? || Digest::SHA1.hexdigest(user.api_secret + t) != token
+        render :json => {:error => 'Invalid Token' }, :status => :unauthorized
+      end
+    end
+  end
+
+  def require_current_user
+    redirect_to auth_github_url if current_user.blank?
+  end
+
+  def current_user
+    if session[:user_id]
+      User.find_by_id(session[:user_id])
+    elsif ! params[:key].blank?
+      User.find_by_api_key(params[:key])
+    else
+      nil
+    end
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,4 +14,28 @@ class ActionController::TestCase
   def sign_in(user)
     session[:user_id] = user.id
   end
+
+  def setup_bogus_controller_routes!
+    begin
+      _routes = Rails.application.routes
+      _routes.disable_clear_and_finalize = true
+      _routes.clear!
+      Rails.application.routes_reloader.paths.each{ |path| load(path) }
+      _routes.draw do
+        match '/:controller(/:action(/:id))'
+      end
+      ActiveSupport.on_load(:action_controller) { _routes.finalize! }
+    ensure
+      _routes.disable_clear_and_finalize = false
+    end
+  end
+
+  def teardown_bogus_controller_routes!
+    Rails.application.reload_routes!
+  end 
+
+  def json_resp
+    ActiveSupport::JSON.decode(@response.body)
+  end
+  
 end

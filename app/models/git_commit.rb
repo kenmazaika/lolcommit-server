43e1b4b697202bff053bed950706ee1e043a33a8
Ken Mazaika <kmazaika@gmail.com>
2012-07-14 13:17:37 -0400

    Temorarily not require the repo, since that isn't quite setup yet.

--- app/models/git_commit.rb
+++ app/models/git_commit.rb
@@ -1,7 +1,7 @@
 class GitCommit < ActiveRecord::Base
   attr_accessible :repo_id, :sha, :image, :email
 
-  validates :repo, :presence => true
+  #validates :repo, :presence => true
   validates :sha,  :presence => true
 
   mount_uploader :image, ImageUploader

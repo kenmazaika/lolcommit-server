6b7fc80c3ec88bf0d9e43e84e810757ca16d950e
Ken Mazaika <kmazaika@gmail.com>
2012-07-17 21:19:48 -0400

    Add instructions to the main page so the app is usable.

--- test/factories.rb
+++ test/factories.rb
@@ -3,7 +3,7 @@ require 'digest/sha1'
 FactoryGirl.define do
   factory :git_commit do
     sha { Digest::SHA1.hexdigest(UUID.generate(:compact)) }
-    email "kenmazaika@gmail.com"
+    association :user
     image "omgimage"
   end
 

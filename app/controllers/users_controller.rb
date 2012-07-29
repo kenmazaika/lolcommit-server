fa6b3778d7c51043fc80624dfab2c3653253242d
Ken Mazaika <kmazaika@gmail.com>
2012-07-28 21:29:25 -0400

    Fix the ordering in the routes file.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_filter :require_current_user, :except => :show
-  before_filter :require_selected_user, :only => :show
+  before_filter :require_selected_user, :only => [:show]
 
   def account
   end

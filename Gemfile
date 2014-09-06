17d3854315f1c96d258028ecd144e0de2fed4be0
Matthew Hutchinson <matt@hiddenloop.com>
2014-09-06 04:23:15 +0100

    fixing test unit issues

--- Gemfile
+++ Gemfile
@@ -3,7 +3,6 @@ source 'https://rubygems.org'
 
 gem 'rails', '4.0.0.rc1'
 gem 'sprockets-rails', :require => 'sprockets/railtie'
-gem 'test-unit'
 
 group :development, :test do
   gem 'pg'
--- db/schema.rb
+++ db/schema.rb
@@ -9,64 +9,66 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120714222426) do
+ActiveRecord::Schema.define(version: 20120714222426) do
 
-  create_table "animated_gifs", :force => true do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "animated_gifs", force: true do |t|
     t.string   "image"
     t.text     "shas"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "git_commits", :force => true do |t|
+  create_table "git_commits", force: true do |t|
     t.string   "image"
     t.string   "sha"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer  "user_id"
     t.integer  "repo_id"
     t.string   "raw"
   end
 
-  add_index "git_commits", ["email"], :name => "index_git_commits_on_email"
-  add_index "git_commits", ["repo_id"], :name => "index_git_commits_on_repo_id"
-  add_index "git_commits", ["sha"], :name => "index_git_commits_on_repo_and_sha"
-  add_index "git_commits", ["sha"], :name => "index_git_commits_on_sha"
-  add_index "git_commits", ["user_id"], :name => "index_git_commits_on_user_id"
+  add_index "git_commits", ["email"], name: "index_git_commits_on_email", using: :btree
+  add_index "git_commits", ["repo_id"], name: "index_git_commits_on_repo_id", using: :btree
+  add_index "git_commits", ["sha"], name: "index_git_commits_on_sha", using: :btree
+  add_index "git_commits", ["user_id"], name: "index_git_commits_on_user_id", using: :btree
 
-  create_table "repos", :force => true do |t|
+  create_table "repos", force: true do |t|
     t.string   "username"
     t.string   "name"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.string   "external_id"
   end
 
-  add_index "repos", ["external_id"], :name => "index_repos_on_external_id"
-  add_index "repos", ["username", "name"], :name => "index_repos_on_username_and_name"
+  add_index "repos", ["external_id"], name: "index_repos_on_external_id", using: :btree
+  add_index "repos", ["username", "name"], name: "index_repos_on_username_and_name", using: :btree
 
-  create_table "repos_users", :id => false, :force => true do |t|
+  create_table "repos_users", id: false, force: true do |t|
     t.integer "repo_id"
     t.integer "user_id"
   end
 
-  add_index "repos_users", ["repo_id", "user_id"], :name => "index_repos_users_on_repo_id_and_user_id"
+  add_index "repos_users", ["repo_id", "user_id"], name: "index_repos_users_on_repo_id_and_user_id", using: :btree
 
-  create_table "users", :force => true do |t|
+  create_table "users", force: true do |t|
     t.string   "name"
     t.integer  "github_id"
     t.string   "email"
     t.string   "token"
     t.string   "api_key"
     t.string   "api_secret"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index "users", ["api_key"], :name => "index_users_on_api_key"
-  add_index "users", ["github_id"], :name => "index_users_on_github_id"
+  add_index "users", ["api_key"], name: "index_users_on_api_key", using: :btree
+  add_index "users", ["github_id"], name: "index_users_on_github_id", using: :btree
 
 end
